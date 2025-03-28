195b5084f3b4d7e7325575154155a2d79176001d
PopoGonry <rhtjdsh35@gmail.com>
2025-03-28 13:53:29 +0900

    project만들기

---
+++ Type casting/practice 4-3/src/Main.java
@@ -0,0 +1,15 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        double doubleValue = 2.71828;
+
+        // double에서 다른 타입으로의 형변환 결과 출력
+        System.out.println("\n=== double 형변환 ===");
+        System.out.println("1. double -> byte: " + (byte)doubleValue);
+        System.out.println("2. double -> short: " + (short)doubleValue);
+        System.out.println("3. double -> int: " + (int)doubleValue);
+        System.out.println("4. double -> long: " + (long)doubleValue);
+        System.out.println("5. double -> float: " + (float)doubleValue);
+    }
+}
---
+++ untitled/src/Main.java
@@ -0,0 +1,14 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+    //1. double(실수형) 로 반지름을 선언
+    //2. 상수로 PI로 선언
+    //3. 계산 = 반지름 * 반지름 * PI
+    //4. 출력
+
+        double half = 10;
+        final double PI = 3.141592;
+        System.out.println(half * half * PI);
+    }
+}
